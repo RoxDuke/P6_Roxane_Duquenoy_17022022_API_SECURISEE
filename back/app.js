d783f36fc65f9d2a9ef25642ca20747c99c6cbb3
RoxDuke <duquenoyroxane11@gmail.com>
2022-02-28 20:15:06 +0100

    Premier ajout du back end

---
+++ back/app.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
+
+const helmet = require('helmet');
+
+const path = require('path');
+
+const saucesRoutes = require('./routes/sauces.route');
+const userRoutes = require("./routes/user.route");
+
+const mongo_pwd = process.env.MONGODB_PASSWORD;
+const mongo_name = process.env.MONGODB_NAME;
+const mongo_data = process.env.MONGODB_DATABESE;
+
+const cors = require("cors");
+let corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+//Connexion à la base de données
+mongoose.connect(`mongodb+srv://Roxane:${mongo_pwd}@${mongo_name}.mongodb.net/${mongo_data}retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+//Permet de sécuriser nos en-têtes HTPP
+app.use(helmet());
+
+//Sécurité CORS, s'appliquant sur toutes les routes
+app.use(function (req, response, next) {
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
+  response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+  next();
+});
+app.use(cors(corsOptions));
+//Gestion des requêtes POST
+app.use(express.json());
+//Enregistrement du routeur
+app.use('/api/auth', userRoutes);
+app.use('/api/sauces', saucesRoutes);
+//Middleware qui sert le dossier images
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+module.exports = app;
---
+++ back/routes/sauces.route.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const sauceCtrl = require('../controllers/sauces.controller');
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+//Création des routes (CRUD: Create Read Update Delete)
+router.get('/', auth, sauceCtrl.getAllStuff);
+router.post('/', auth, multer, sauceCtrl.createThing);
+router.get('/:id', auth, sauceCtrl.getOneThing);
+router.put('/:id', auth, multer, sauceCtrl.modifyThing);
+router.delete('/:id',auth, sauceCtrl.deleteThing);
+
+module.exports = router;

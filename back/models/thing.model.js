26de6fc5bef6bda5fe3dbccc8d33b3b7d742a94e
RoxDuke <duquenoyroxane11@gmail.com>
2022-03-01 17:38:44 +0100

    Modification du modèle des sauces

--- back/models/thing.model.js
+++ back/models/thing.model.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 
 const thingSchema = mongoose.Schema({
-    userId: { type: String, required: true},
+    userId: { type: String, required: true },
     name: { type: String, required: true },
     manufacturer: { type: String, required: true },
     description: { type: String, required: true },
-    mainPepper: { type: String, required: true},
+    mainPepper: { type: String, required: true },
     imageUrl: { type: String, required: true },
     heat: { type: Number, required: true },
-    likes: { type: Number, required: true },
-    dislikes: { type: Number, required: true },
-    usersLiked: { type: String, required: true },
-
+    likes: { type: Number, default: 0, required: true },
+    dislikes: { type: Number, default: 0, required: true },
+    usersLiked: { type: Array, default: [], required: true },
+    usersDisliked: { type: Array, default: [], required: true },
 });
 
 module.exports = mongoose.model('Thing', thingSchema);
